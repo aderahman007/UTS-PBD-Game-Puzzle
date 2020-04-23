b455e9e051715e8916f9b0e81a1dbd03e8ef54cf
aderahman007 <aderahman9908@gmail.com>
2020-04-23 12:33:28 +0700

    Firts Commit and Finish Project

---
+++ app/src/main/java/com/aderahman/gamepuzzleutspbd/MainActivity.java
@@ -0,0 +1,52 @@
+package com.aderahman.gamepuzzleutspbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView tentang, main;
+    Intent intent;
+
+    MediaPlayer audiobacksound;
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        audiobacksound = MediaPlayer.create(this, R.raw.intro);
+        audiobacksound.setLooping(true);
+        audiobacksound.setVolume(1,1);
+        audiobacksound.start();
+
+        tentang = findViewById(R.id.tentang);
+        tentang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, TentangActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        main = findViewById(R.id.main);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+                audiobacksound.stop();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/aderahman/gamepuzzleutspbd/TentangActivity.java
@@ -0,0 +1,29 @@
+package com.aderahman.gamepuzzleutspbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class TentangActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        imageView = findViewById(R.id.back);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
